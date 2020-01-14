b6d5ac1c2f0bafb50f30bd73fb2078a79efa9382
faisalsrs <faisalsrs@gmail.com>
2020-01-13 17:43:10 -0800

    Time_Display

---
+++ Time_Display/server/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\faisa\documents\coding dojo\python\django\time_display\server\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
